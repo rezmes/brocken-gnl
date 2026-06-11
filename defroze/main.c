fc4d147882237823cd97d2e582a113350e71297e
mmesgari <mmesgari@f6r11s15.paris.42.school>
2026-06-11 19:02:28 +0200

    defroze ft_strncat

--- defroze/main.c
+++ defroze/main.c
@@ -101,6 +101,19 @@ char *ft_strcat(char *dest, char *src)
 //    return (start);
 }
 
+char *ft_strncat(char *dest, char *src, int n)
+{
+    int i = 0;
+    int j = 0;
+
+    while (dest[i])
+        i++;
+    while (src[j] && j < n)
+        dest[i++] = src[j++];
+    dest[i] = '\0';
+    return (dest);
+}
+
 #include <stdio.h>
 int main(void)
 {
